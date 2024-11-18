afc0faae9e0c47b6d54e8bbcadb9cf78a6514242
Gabriel C. de Souza <gabrielsouza_1909@hotmail.com>
2024-11-18 16:36:52 -0300

    teste evio de  e-mail

--- sendReport.js
+++ sendReport.js
@@ -1,40 +1,43 @@
-const nodemailer = require('nodemailer');
-const fs = require('fs');
-const path = require('path');
+const nodemailer = require('nodemailer')
+const fs = require('fs')
+
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: 'dev.gbrlsouzal@gmail.com',
-    pass: 'hsqwlpypyierinxc'
+    pass: 'hsqw lpyp yier inxc'
   }
-});
+})
 
-const reportPath = path.join(__dirname, 'cypress', 'reports', 'index.html');
+const reportPath = path.join(__dirname, 'cypress', 'reports', 'index.html')
+const reportContent = fs.readFileSync(reportPath, 'utf-8');
 
 fs.readFile(reportPath, (err, data) => {
   if (err) {
-    console.error('Erro ao ler o relatório:', err);
-    return;
+    console.error('Erro ao ler o relatório:', err)
+    return
   }
 
   const mailOptions = {
     from: 'dev.gbrlsouza@gmail.com',
     to: 'dev.gbrlsouza@gmail.com',
-    subject: 'Relatório de Teste Cypress',
-    text: 'Segue em anexo o relatório dos testes automatizados executados no Cypress.',
+    subject: 'Relatório de Testes Cypress',
+    html: `<h3>Segue o relatório de testes do Cypress</h3>`,
     attachments: [
       {
         filename: 'relatorio-cypress.html',
-        content: data
+        content: reportContent
       }
     ]
-  };
+  }
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.log('Erro ao enviar o e-mail:', error);
+      console.log('Erro ao enviar o e-mail:', error)
     } else {
-      console.log('E-mail enviado:', info.response);
+      console.log('E-mail enviado:', info.response)
     }
-  });
-});
+  })
+})
