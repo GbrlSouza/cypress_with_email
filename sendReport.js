92f2092a6cfadd837673fc71c4cb1f4dd622425b
Gabriel C. de Souza <gabrielsouza_1909@hotmail.com>
2024-11-19 16:35:30 -0300

    imap && smtp + html && pdf

--- sendReport.js
+++ sendReport.js
@@ -5,10 +5,80 @@ const path = require('path')
 const fs = require('fs')
 const { create } = require('html-pdf-chrome')
 
+async function ensureDirectoryExists(directoryPath) {
+  try {
+    await fs.promises.mkdir(directoryPath, { recursive: true })
+    console.log(`Diretório criado ou já existe: ${directoryPath}`)
+  } catch (error) { console.error('Erro ao criar diretório:', error) }
+}
+
+async function sendEmailWithAttachment(pdfPath) {
+  const mailOptions = {
+    from: process.env.EMAIL,
+    to: 'gabrielsouza_1909@hotmail.com',
+    subject: 'Relatório de Testes Cypress',
+    html: '<h3>Segue o relatório de testes do Cypress</h3>',
+
+    attachments: [
+      {
+        filename: 'relatorio-cypress.pdf',
+        path: pdfPath,
+      },
+      {
+        filename: 'video.mp4', 
+        path: videoPath, 
+      },
+    ],
+  }
+
+  try {
+    const info = await transporter.sendMail(mailOptions)
+    console.log('E-mail enviado com sucesso:', info.response)
+  } catch (error) { console.error('Erro ao enviar o e-mail:', error) }
+}
+
+async function checkCypressReportForErrors(htmlFilePath) {
+  try {
+    const htmlContent = await fs.promises.readFile(htmlFilePath, 'utf-8')
+    return htmlContent.includes('failed')
+  } catch (error) {
+    console.error('Erro ao ler o relatório do Cypress:', error)
+    return false
+  }
+}
+
+async function convertHtmlToPdf(htmlFilePath, outputPdfPath) {
+  try {
+    const htmlContent = await fs.promises.readFile(htmlFilePath, 'utf-8')
+
+    const options = {
+      launchOptions: {
+        headless: true,
+        executablePath: '/usr/bin/google-chrome',
+      },
+
+      printOptions: {
+        format: 'A4',
+        landscape: true,
+        printBackground: true,
+        preferCSSPageSize: true
+      },
+    }
+
+    const pdf = await create(htmlContent, options)
+    
+    setTimeout(async ()=> {
+      await pdf.toFile(outputPdfPath)
+      console.log('Relatório convertido para PDF com sucesso:', outputPdfPath)
+    }, 25000)
+  } catch (error) { console.error('Erro ao gerar o PDF:', error) }
+}
+
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
   secure: true,
+
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASSWORD,
@@ -16,59 +86,23 @@ const transporter = nodemailer.createTransport({
 })
 
 const reportsDir = path.resolve(__dirname, 'cypress/reports')
-const reportPath = path.join(reportsDir, 'html/index.html')
+const reportPath = path.join(reportsDir, 'html', 'index.html')
 const pdfDir = path.join(reportsDir, 'pdf')
 const pdfPath = path.join(pdfDir, 'relatorio-cypress.pdf')
+const videoDir = path.join(reportsDir, 'html/videos')
+const videoPath = path.join(videoDir, 'todo.cy.js.mp4')
 
-function ensureDirectoryExists(directoryPath) {
-  if (!fs.existsSync(directoryPath)) {
-    fs.mkdirSync(directoryPath, { recursive: true })
-    console.log(`Diretório criado: ${directoryPath}`)
-  }
-}
+ensureDirectoryExists(pdfDir).then(async () => {
+  const hasErrors = await checkCypressReportForErrors(reportPath)
 
-ensureDirectoryExists(pdfDir)
+  if (hasErrors) {
+    await convertHtmlToPdf(reportPath, pdfPath)
 
-async function convertHtmlToPdf(htmlFilePath, outputPdfPath) {
-  try {
-    const htmlContent = fs.readFileSync(htmlFilePath, 'utf-8')
-    const options = {
-      launchOptions: {
-        headless: true,
-        executablePath: '/usr/bin/google-chrome',
-      },
-      printOptions: {
-        format: 'A4', 
-        printBackground: true,
-      },
+    if (!fs.existsSync(pdfPath)) {
+      console.error('Erro: O arquivo PDF não foi gerado corretamente.')
+      return
     }
 
-    await create(htmlContent, options).then((pdf) => pdf.toFile(outputPdfPath))
-    console.log('Relatório convertido para PDF com sucesso:', outputPdfPath)
-  } catch (error) { console.error('Erro ao gerar o PDF:', error) }
-}
-
-convertHtmlToPdf(reportPath, pdfPath).then(() => {
-  if (!fs.existsSync(pdfPath)) {
-    console.error('Erro: O arquivo PDF não foi gerado corretamente.')
-    return
-  }
-
-  const mailOptions = {
-    from: process.env.EMAIL,
-    to: 'gabrielsouza_1909@hotmail.com',
-    subject: 'Relatório de Testes Cypress',
-    html: '<h3>Segue o relatório de testes do Cypress</h3>',
-    attachments: [
-      {
-        filename: 'relatorio-cypress.pdf',
-        path: pdfPath,
-      },
-    ],
-  }
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) { console.log('Erro ao enviar o e-mail:', error) }
-    else { console.log('E-mail enviado:', info.response) }
-  })
+    await sendEmailWithAttachment(pdfPath)
+  } else { console.log('Nenhum erro encontrado nos testes do Cypress. Nenhum e-mail enviado.') }
 })
